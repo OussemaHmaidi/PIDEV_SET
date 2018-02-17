9a2f0edc6d3ff76472e2d7dca6dbcb3bb25fe9c1
user <user@mbp-de-user.home>
2018-02-17 05:48:24 +0100

    new commit

--- src/esprit/pidev/gui/IndexParentController.java
+++ src/esprit/pidev/gui/IndexParentController.java
@@ -5,9 +5,20 @@
  */
 package esprit.pidev.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +27,17 @@ import javafx.fxml.Initializable;
  */
 public class IndexParentController implements Initializable {
 
+    @FXML
+    private AnchorPane topbar;
+    @FXML
+    private ImageView setting;
+    @FXML
+    private Pane settingpane;
+    @FXML
+    private Label settinglabel;
+    @FXML
+    private Label logoutlabel;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +45,23 @@ public class IndexParentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void setting(MouseEvent event) {
+        
+        if(settingpane.isVisible()){ settingpane.setVisible(false);}
+        else {settingpane.setVisible(true);}
+    }
+
+    @FXML
+    private void logout(MouseEvent event) throws IOException {
+        
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+            
+            Stage stage = new Stage();            
+            stage.setTitle("All4Kids");
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Connexion.fxml"))));
+            stage.show();
+    }
     
 }
